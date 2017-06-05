a6f6e29664f23857708657489cf49dcc5451c2c2
Vashist KS <Vashist KS>
2017-06-05 01:33:13 -0400

    Clear file

--- public/js/service/services.js
+++
@@ -1,7 +0,0 @@
-movieStubApp = angular.module('movieStubApp', ['ngResource', 'ngRoute']);
-movieStubApp.factory('movieStubFactory', function ($resource) {
-    return $resource('/movies');
-});
-movieStubApp.factory('movieStubBookingsFactory', function ($resource) {
-    return $resource('/bookings');
-});
